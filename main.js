b6fd50fd52e30bcae0006695c8e9726295365eef
aelgy <aelgy333@gmail.com>
2024-04-16 10:58:03 +0800

    add overlay

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
+const overlay = document.getElementById('overlay')
 const modal = document.getElementById('modal')
 const login = document.getElementById('login-modal')
 const post = document.getElementById('post-modal')
 
 function loginModal() {
+  overlay.style.display = 'block'
   modal.style.display = 'block';
   login.style.display = 'block';
 }
 
 function postModal() {
+  overlay.style.display = 'block'
   modal.style.display = 'block';
   post.style.display = 'block';
 }
 
 function closeBtn() {
+  overlay.style.display = 'none'
   modal.style.display = 'none';
   login.style.display = 'none';
   post.style.display = 'none';
